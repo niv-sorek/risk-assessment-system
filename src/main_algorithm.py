17d6be3b4ad58c70c6b0dd778e05be985837427a
Niv <sorekniv@gmail.com>
2020-12-29 17:04:27 +0200

    TO-DO_LIST

--- src/main_algorithm.py
+++ src/main_algorithm.py
@@ -3,12 +3,16 @@ from src import utils
 
 xiaomi = Component("microsoft", "Windows", "10")
 print(str(xiaomi))
-f'''
+'''
 1. Read From JSON all components & calculate components Risk + add to list
-
 2. Read from JSON all users & create a list of Components
+3. Loop over UserComponents (List) to match with Component (By unique ID) 
+4. Calculate user's level by its UserComponents
 
-3. Loop over UserComponents (List) to match with Component (By unique ID) -
+
+// Later:
+6. React Client
+7. HTTPServer
 
 '''
 # TODO
